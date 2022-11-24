656d59ddc696e768dca8360a4ed03670d0a8abb6
Paul Pottorff <8058447+ppottorff@users.noreply.github.com>
2022-11-24 22:26:09 +0000

    Update container tools and plugins

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
-# stuff to do on container before using it
+# stuff to install on container before using it
 sudo apt update -y
 sudo apt upgrade -y
-// sudo apt install awscli -y - ADDED to features
-// sudo apt install nodejs -y - ADDED to features
-// npm install -g npm@9.1.1 SEE if needed
-// npm install -g aws-cdk -y ADDED to features
+sudo apt install nodejs -y
+sudo apt install awscli -y
+# like to install a lot of this with npm
+npm install -g npm@9.1.1 -y
+npm install -g aws-cdk -y 
+npm install -g typescript -y
+npm install -g react-cli react -y
+# npm install -g @aws-amplify/cli -y
+# npm install -g @angular/cli -y
+sudo sh -c "echo 'deb https://gitsecret.jfrog.io/artifactory/git-secret-deb git-secret main' >> /etc/apt/sources.list"
+wget -qO - 'https://gitsecret.jfrog.io/artifactory/api/gpg/key/public' | sudo apt-key add -
+sudo apt-get update && sudo apt-get install -y git-secret
+git secret --version
