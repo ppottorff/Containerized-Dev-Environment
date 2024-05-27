96269fcfff41c3a3d66918622b75fb3daed8ed6a
Paul Pottorff <8058447+ppottorff@users.noreply.github.com>
2024-05-27 18:25:05 +0000

    Fixing trivy action, adding dependency review.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,7 +2,7 @@
 # stuff to do on container before using it
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install awscli -y
-sudo apt install nodejs -y
-npm install -g npm@9.1.1
-npm install -g aws-cdk
+# sudo apt install awscli -y
+# sudo apt install nodejs -y
+# npm install -g npm@9.1.1
+# npm install -g aws-cdk
