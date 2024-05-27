482ef6bc5ff80c4e0728df0aea24a5e069e00bb7
Paul Pottorff <8058447+ppottorff@users.noreply.github.com>
2024-05-27 18:56:20 +0000

    simple updates to add codeql and run dependency-review

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # stuff to do on container before using it
-sudo apt update -y
-sudo apt upgrade -y
+# sudo apt update -y
+# sudo apt upgrade -y
 # sudo apt install awscli -y
 # sudo apt install nodejs -y
 # npm install -g npm@9.1.1
